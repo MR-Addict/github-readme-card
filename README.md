698a52926eda9904c97703a80221464d2c457076
MR-Addict <2750417853@qq.com>
2023-03-17 12:14:15 +0800

    shrink repo cards to 2

--- README.md
+++ README.md
@@ -5,8 +5,6 @@
 ### Repository cards:
 
 [![github readme card](https://raw.githubusercontent.com/MR-Addict/github-readme-card/output/repocards/github-readme-card.svg)](https://github.com/MR-Addict/github-readme-card/)
-[![useless-box](https://raw.githubusercontent.com/MR-Addict/github-readme-card/output/repocards/useless-box.svg)](https://github.com/MR-Addict/useless-box/)
-[![Svelte](https://raw.githubusercontent.com/MR-Addict/github-readme-card/output/repocards/svelte.svg)](https://github.com/sveltejs/svelte/)
 [![Next.js](https://raw.githubusercontent.com/MR-Addict/github-readme-card/output/repocards/next.js.svg)](https://github.com/vercel/next.js/)
 
 ## 2. Development
--- src/export/exportAll.ts
+++ src/export/exportAll.ts
@@ -1,28 +1,6 @@
 import "module-alias/register";
 import "@/loadenv";
 
-import fs from "fs";
-import path from "path";
-
-import { RepoConfigType } from "@/types";
-import { compileRepoCard } from "@/svg";
-
-async function readRepos() {
-  const fullPath = path.join(process.cwd(), "config.json");
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  return JSON.parse(fileContents).repos as RepoConfigType[];
-}
-
-export default async function exportRepoCards() {
-  const build_dir = path.join(process.cwd(), "output/repocards");
-  fs.mkdirSync(build_dir, { recursive: true });
-
-  const repos = await readRepos();
-  const repoStrings = await Promise.all(repos.map((item) => compileRepoCard(item.user, item.repo)));
-
-  await Promise.all(
-    repoStrings.map((item, index) => fs.promises.writeFile(path.join(build_dir, repos[index].repo + ".svg"), item))
-  );
-}
+import exportRepoCards from "./exportRepoCards";
 
 exportRepoCards();
